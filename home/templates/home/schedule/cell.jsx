33fe20f41051d67d31c2c2f548a5c647e8ff0cfc
Stanislav Khramtsov <sauron4er@gmail.com>
2022-05-17 07:34:29 +0300

    work

--- home/templates/home/schedule/cell.jsx
+++ home/templates/home/schedule/cell.jsx
@@ -4,7 +4,6 @@ import {store, view} from '@risingstack/react-easy-state';
 import schedulerState from 'home/templates/home/schedule/state';
 import './schedule.css';
 
-
 function Cell(props) {
   const [state, setState] = useSetState({});
 
@@ -13,9 +12,19 @@ function Cell(props) {
     schedulerState.clicked_time = props.time;
   }
 
-  return <td className='scheduler_td' onClick={onClick}>
-    <If condition={props.visits.length > 0}>!</If>
-  </td>;
+  // TODO Чому візит записується на годину пізніше??
+
+  return (
+    <td className='scheduler_td' onClick={onClick}>
+      <If condition={props.visits.length > 0}>
+        <div className='visits_container' onClick={() => {}}>
+          <For each='visit' of={props.visits} index='index'>
+            !!
+          </For>
+        </div>
+      </If>
+    </td>
+  );
 }
 
 Cell.defaultProps = {
